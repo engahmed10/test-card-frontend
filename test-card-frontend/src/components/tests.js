8d22f5f93c6a1d6686219e26095504dec122b8b2
AHMED <eng.computer1@yahoo.com>
2021-03-18 22:35:10 -0500

    useeffect &fetched data from backend

--- test-card-frontend/src/components/tests.js
+++ test-card-frontend/src/components/tests.js
@@ -1,18 +1,36 @@
-import {useEffect}  from "react" 
+
+import {useEffect,useState}  from "react" 
+
 
 
 function Tests() {
 
+
+  const [tests,setTests] = useState()
+
 useEffect(()=>{
-   fetch(URL)
-   .then(response=>response.json())
-   .then(data=>console.log(`data`,data))
+
+   const url="http://127.0.0.1:3000/tests"
+   fetch(url)
+   .then((response) => response.json())
+   .then(data=>{setTests(data)})
+     .catch((error) => console.log(error));
 
 },[])
 
+
   return (
     <div>
-      All tests here
+    { tests !== undefined?
+    
+      tests.map(test=>
+          { return (test.question,
+            test.answer)
+           }
+      ):
+      ""
+    }
+
     </div>
   );
 }
