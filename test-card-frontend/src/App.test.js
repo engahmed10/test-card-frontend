028e5e992f2582ce2201619281b3b59c80da5eae
AHMED <eng.computer1@yahoo.com>
2021-04-01 23:15:29 -0500

    added jest test to chcek  is AddTest component is correct or not

--- test-card-frontend/src/App.test.js
+++ test-card-frontend/src/App.test.js
@@ -1,21 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import ReactDOM from 'react-dom'
+import AddTest  from './components/addtests'
+
+test('Shows that he contains about component and chcek is the app is true no error',()=>{
 
 
-test('Shows a About first',()=>{
+      const div = document.createElement('div');
 
 
-const div = document.createElement('div');
+      ReactDOM.render(<App/>,div);
 
 
+      //if we wan to find About in App component  <Route exact path="/About"  />,we will use toContain and test will pass 
+      expect(div.innerHTML).toContain('About');
+      //check is it true or false
+      expect('div.innerHTML').toBeTruthy();
+      ReactDOM.unmountComponentAtNode(div);
 
-ReactDOM.render(<App/>,div);
+})
 
 
-//if we wan to find about in App component  <Route exact path="/About"  render={routeProps => <About {...routeProps} /> }/>
-expect(div.innerHTML).toContain('About');
+test('checks how is working ',()=>{
+  
+  const div = document.createElement('div')
+  
+  ReactDOM.render(<AddTest/>,div);
 
-ReactDOM.unmountComponentAtNode(div);
+ //expect('div.innerHTML').toContain('Form');
+ expect('div.innerHTML').toBeTruthy();
 
 })
